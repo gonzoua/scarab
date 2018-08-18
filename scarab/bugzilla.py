3bfcb5ef7b399b3712f2ffd5304ca5c4b87c1011
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2018-08-18 16:57:44 -0700

    Implement files submcommand

--- scarab/bugzilla.py
+++ scarab/bugzilla.py
@@ -1,6 +1,30 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import xmlrpc.client
+from datetime import datetime
+from datetime import timezone
+
+class Attachment(object):
+    def __init__(self, d):
+        self.object_id = int(d['id'])
+        self.bug_id = int(d['bug_id'])
+        self.file_name = d['file_name']
+        self.summary = d['summary']
+        self.content_type = d['content_type']
+        self.size = int(d['size'])
+        time_tuple = d['creation_time'].timetuple()
+        # Convert from UTC to local timezone
+        dt = datetime(*time_tuple[0:6], tzinfo=timezone.utc).astimezone(tz=None)
+        self.creation_time = dt
+        time_tuple = d['last_change_time'].timetuple()
+        # Convert from UTC to local timezone
+        dt = datetime(*time_tuple[0:6], tzinfo=timezone.utc).astimezone(tz=None)
+        self.last_change_time = dt
+        self.creator = d['creator']
+        self.is_obsolete = d['is_obsolete']
+
+    def __repr__(self):
+        return ("Attachment(%d, '%s')" % (self.object_id, self.file_name))
 
 class Bugzilla(object):
     __token = None
@@ -25,12 +49,19 @@ class Bugzilla(object):
         result = self.__proxy.Bug.get(args)
         return result
 
-    def attachments(self, bug_id):
+    def attachments(self, bug_id, include_obsolete=False):
         args = self.common_args()
         args['ids'] = [bug_id]
         # Do not requets attachment data
         args['exclude_fields'] = ['data']
-        result = self.__proxy.Bug.attachments(args)
+        reply = self.__proxy.Bug.attachments(args)
+        result = []
+        for d in reply['bugs'][str(bug_id)]:
+            result.append(Attachment(d))
+        if not include_obsolete:
+            result = [a for a in result if not a.is_obsolete]
+        result.sort(key=lambda a: a.object_id)
+
         return result
 
     def attachment(self, attachment_id):
--- scarab/commands/files.py
+++ scarab/commands/files.py
@@ -1,17 +1,56 @@
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
 from context import bugzilla_instance
 
 from json import dumps
 from .base import Base
+from datetime import datetime, timezone
 
 class Command(Base):
-    """Attach file to the existing PR"""
+    """List files attached to specified PR"""
 
     def register(self, subparsers):
         parser = subparsers.add_parser('files')
         parser.set_defaults(func=self.run)
         parser.add_argument('bug_id', type=int, help='Bug ID')
         parser.add_argument('-a', '--all', action='store_true', help='show all attachments (including obsolete)')
+        parser.add_argument('-s', '--summary', action='store_true', help='show summary instead of file name')
+
+    def __format_date(self, date):
+        delta =  datetime.utcnow().replace(tzinfo=timezone.utc) - date
+        if delta.days < 364:
+            return date.strftime('%b %d %H:%M')
+        else:
+            return date.strftime('%Y %b %d')
 
     def run(self, args):
         bugzilla = bugzilla_instance()
-        print (bugzilla.attachments(args.bug_id))
+        attachments = bugzilla.attachments(args.bug_id, args.all)
+
+        rows = []
+        for a in attachments:
+            row = []
+            row.append(str(a.object_id))
+            row.append(a.creator)
+            row.append(str(a.size))
+            row.append(self.__format_date(a.creation_time))
+            if args.all:
+                row.append('O' if a.is_obsolete else '')
+            row.append(a.summary if args.summary else a.file_name)
+            rows.append(row)
+
+        # find width for every columen except last one
+        # it's either file name or summary so should not be limited
+        if len(rows) > 0:
+            r = 0
+            column_formats = []
+            for r in range(len(rows[0]) - 1):
+                width = max([len(str(row[r])) for row in rows])
+                column_format = '{: >%d}' % width
+                column_formats.append(column_format)
+            row_format = '  '.join(column_formats)
+            row_format += ' {}'
+            for row in rows:
+                print (row_format.format(*row))
+
+        # print("{: >7} {: >20}".format(a.object_id, a.file_name))
