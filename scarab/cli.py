5638df1a7de1e6d197fe5ab3de7e43c6fe76969f
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2018-08-26 12:13:46 -0700

    pylint scrubbing: add docstring and remove unused import

--- scarab/cli.py
+++ scarab/cli.py
@@ -1,9 +1,9 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 from commands import create_parser
-from settings import Settings
 
 def main():
+    """Main entry point for scarab CLI"""
     parser = create_parser()
     args = parser.parse_args()
     args.func(args)
