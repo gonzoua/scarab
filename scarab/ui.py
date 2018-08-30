d9465d91ce8478a775f02a087bc3cf8c0bcd8fb3
Oleksandr Tymoshenko <oleksandr.tymoshenko@dolby.com>
2018-08-30 13:15:37 -0700

    Add edit_message function

--- scarab/ui.py
+++ scarab/ui.py
@@ -2,7 +2,13 @@
 """
 Module handles all the interactoins with user
 """
-import sys
+import sys, tempfile, os
+from subprocess import call
+
+MESSAGE = '''
+# SCARAB: Please enter the comment. Lines starting with '# SCARAB:'
+# SCARAB: will be ignored
+'''
 
 def fatal(line):
     """
@@ -18,3 +24,32 @@ def log(line):
 def output(line):
     """Output normal message"""
     sys.stdout.write('{}\n'.format(line))
+
+def edit_message(initial_message = None):
+    """
+    Invoke editor to let user provide multi-line input. All lines starting
+    with '# SCARAB:' are considered internal comments and ignored. Leading
+    and trailing spaces are removed from final comment
+    """
+    editor = os.environ.get('SCARAB_EDITOR', None)
+    if editor is None:
+        editor = os.environ.get('EDITOR', 'vi')
+
+    tf = tempfile.NamedTemporaryFile(suffix=".tmp", mode="w+", delete=False)
+    if not initial_message is None:
+        tf.write(initial_message)
+    tf.write(MESSAGE)
+    tf.flush()
+    name = tf.name
+    tf.close()
+
+    call([editor, name])
+
+    with open(name, 'r') as tf:
+        message = tf.read()
+
+    lines = message.split('\n')
+    lines = [l for l in lines if not l.startswith('# SCARAB:')]
+    message = '\n'.join(lines).strip()
+
+    return message
